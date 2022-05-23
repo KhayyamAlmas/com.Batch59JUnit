891c8ca9cc23983f9fc6da6efab27afe7feca643
abuee <khayyam.a.a@gmail.com>
2022-05-23 12:38:44 +0400

    23-05-2022

--- src/test/java/day11_faker_file/C01_Faker.java
+++ src/test/java/day11_faker_file/C01_Faker.java
@@ -1,5 +1,6 @@
-package day10_actions;
+package day11_faker_file;
 
+import com.github.javafaker.Faker;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -7,15 +8,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
-public class C06_KeyboardActions extends TestBase {
+public class C01_Faker extends TestBase {
 
     @Test
-    public void test() throws InterruptedException {
-
-
-
-
-
+    public void test01(){
+        // facebook gorevini fake isimlerle yapalim
         // facebook anasyafaya gidip
         driver.get("https://www.facebook.com");
 
@@ -23,20 +20,24 @@ public class C06_KeyboardActions extends TestBase {
         driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
 
         // isim kutusunu locate edip
-       WebElement isimKutusu= driver.findElement(By.xpath("//input[@name='firstname']"));
+        WebElement isimKutusu= driver.findElement(By.xpath("//input[@name='firstname']"));
 
         // geriye kalan alanlari TAB ile dolasarak
         // formu doldurun
 
         Actions actions = new Actions(driver);
+        Faker faker = new Faker();
+        String fakeMail=faker.internet().emailAddress();
         actions.click(isimKutusu).
-                sendKeys("Khayyam").
+                sendKeys(faker.name().firstName()).
                 sendKeys(Keys.TAB).
-                sendKeys("Almas").
+                sendKeys(faker.name().lastName()).
                 sendKeys(Keys.TAB).
-                sendKeys("0123456987").
+                sendKeys(fakeMail).
                 sendKeys(Keys.TAB).
-                sendKeys("123456789ax").
+                sendKeys(faker.internet().password()).
+                sendKeys(Keys.TAB).
+                sendKeys(faker.internet().password()).
                 sendKeys(Keys.TAB).
                 sendKeys(Keys.TAB).
                 sendKeys("26").
@@ -54,9 +55,5 @@ public class C06_KeyboardActions extends TestBase {
                 sendKeys(Keys.ENTER).
                 perform();
 
-
-
-
-        Thread.sleep(5000);
     }
 }

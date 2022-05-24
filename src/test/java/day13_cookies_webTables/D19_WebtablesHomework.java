3718caddad981e1b30cac7173ce8f05e02f13f31
abuee <khayyam.a.a@gmail.com>
2022-05-24 22:34:08 +0400

    23-05-2022

---
+++ src/test/java/day13_cookies_webTables/D19_WebtablesHomework.java
@@ -0,0 +1,21 @@
+package day13_cookies_webTables;
+
+import org.junit.Test;
+
+public class D19_WebtablesHomework {
+
+    @Test
+    public void name() {
+        // Bir Class olusturun D19_WebtablesHomework
+        // 1. “https://demoqa.com/webtables” sayfasina gidin
+        // 2. Headers da bulunan department isimlerini yazdirin
+        // 3. sutunun basligini yazdirin
+        // 4. Tablodaki tum datalari yazdirin
+        // 5. Tabloda kac cell (data) oldugunu yazdirin
+        // 6. Tablodaki satir sayisini yazdirin
+        // 7. Tablodaki sutun sayisini yazdirin
+        // 8. Tablodaki 3.kolonu yazdirin
+        // 9. Tabloda "First Name" i Kierra olan kisinin Salary'sini yazdirin
+        // 10. Page sayfasinda bir method olusturun, Test sayfasindan satir ve sutun sayisini girdigimde bana datayi yazdirsin
+    }
+}
